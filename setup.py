a66b509546c617db46c3eda7a68c01c6415aab94
Ahmed Dhankwala <ahmeddhankwala786@gmail.com>
2025-12-12 13:31:09 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name = 'ML_project',
+version= '0.0.1',
+author='Krish',
+author_email='ahmeddhankwala786@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
